4bcf0f5a1880ea262a927a758dbc83e9b95ae2be
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-09-14 16:15:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,4 +13,7 @@ def introduction (name)
     puts "Hi my name is #{name} and I am learning to porgram in #{language}. "
   end 
   introduction_with_language("Dan", "Ember")
+  def introduction_with_language (name, language)
+    puts "Hi my name is #{name} and I am learning to porgram in #{language}. "
+  end 
   
